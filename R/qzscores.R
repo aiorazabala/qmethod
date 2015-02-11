8a8f71c66d4d971aa530a3a59a2cbfa6a68db41c
Maximilian Held <maximilian.held83@gmail.com>
2015-02-11 21:31:43 +0100

    add item dispersion (pop sd) as per #66

--- R/qzscores.R
+++ R/qzscores.R
@@ -3,10 +3,11 @@ qzscores <- function(dataset, nfactors, loa=loa, flagged=flagged, forced=TRUE, d
   # calculate number of Q sorts and number of statements
   nstat <- nrow(dataset)
   nqsorts <- ncol(dataset)
+  pop.sd <- function(x)(sqrt(var(x)*(length(x)-1)/length(x)))
   #A. select FLAGGED Q sorts
   floa <- flagged*loa #as.data.frame(loa); floa[which(!flagged, arr.ind=T)] <- 0 # the latter does not work in old versions of R
   #B. calculate FACTOR WEIGHTS for each Q sort, in a new matrix -needs to be a data.frame to perform variable calculations
-  fwe <- as.data.frame(apply(floa, 2, function(x) x/(1-x^2)))
+  fwe <- as.data.frame(apply(floa, 2, function(x) x/(1-x^2))) # this the formula from from Brown 1980: 242
   #C. calculate Z-SCORES for each sentence and factor 
   #-- new matrix for wsubm*ssubmn (original matrix * q sort factor weight), and transpose
   wraw_all <- list()
@@ -18,22 +19,29 @@ qzscores <- function(dataset, nfactors, loa=loa, flagged=flagged, forced=TRUE, d
     n <- n+1
   }
   #-- sums, average and stdev for each statement
+  #-- also itemsd, the standard dev of each statement across all flagged qsorts
   zsc_sum <- data.frame(cbind(1:nstat))
   zsc_mea <- data.frame(cbind(1:nstat))
-  zsc_std <- data.frame(cbind(1:nstat))
+  zsc_std <- data.frame(cbind(1:nstat))  # for zscoring, sd across all items per factor
+  item_sd <- data.frame(cbind(1:nstat))  # for viz, sd across all qsorts on an item per factor
   row.names(zsc_sum) <- row.names(dataset)
   row.names(zsc_mea) <- row.names(dataset)
   row.names(zsc_std) <- row.names(dataset)
+  row.names(item_sd) <- row.names(dataset)
   n <- 1
   while (n <= length(floa)) {
     zsc_sum[,n] <-      rowSums(wraw_all[[n]])
     zsc_mea[,n] <- mean(rowSums(wraw_all[[n]]))
-    zsc_std[,n] <-   sd(rowSums(wraw_all[[n]]))
+    zsc_std[,n] <-   sd(rowSums(wraw_all[[n]]))  # again, this is the sd across all items per factor
+    # here comes the sd across all sorts flagged on a factor
+    wraw_all_flagged <- wraw_all[[n]][,which(!apply(wraw_all[[n]]==0,2,all))]  # chose only flagged
+    item_sd[,n] <- apply(wraw_all_flagged,1,pop.sd) # make pop sd
     n <- n+1
   }
   colnames(zsc_sum) <- paste("z_sum_",c(1:length(floa)),sep="")
   colnames(zsc_mea) <- paste("z_mea_",c(1:length(floa)),sep="")
   colnames(zsc_std) <- paste("z_std_",c(1:length(floa)),sep="")
+  colnames(item_sd) <- paste("item_sd_",c(1:length(floa)),sep="")
   #-- z-scores for each statement
   zsc <- data.frame(cbind(1:nstat))
   row.names(zsc) <- row.names(dataset)
@@ -94,7 +102,9 @@ qzscores <- function(dataset, nfactors, loa=loa, flagged=flagged, forced=TRUE, d
   qmethodresults[[5]] <- zsc
   qmethodresults[[6]] <- zsc_n
   qmethodresults[[7]] <- f_char
-  names(qmethodresults) <- c("brief", "dataset", "loa", "flagged", "zsc", "zsc_n", "f_char")
+  qmethodresults[[9]] <- item_sd  # 8 is qdc!
+  names(qmethodresults) <- c("brief", "dataset", "loa", "flagged", "zsc", "zsc_n", "f_char","item_sd")
   class(qmethodresults) <- "QmethodRes"
   return(qmethodresults)
 }
+
