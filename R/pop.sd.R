6615412181c19bda32a56bc2fe8d194a50fdbb45
Maximilian Held <maximilian.held83@gmail.com>
2015-05-16 15:14:01 +0200

    implement, document pop.sd() helper function closes https://github.com/aiorazabala/qmethod/issues/126

--- R/pop.sd.R
+++ R/pop.sd.R
@@ -1,3 +1,8 @@
 pop.sd <- function(x, ...){
+  # this is just a simple helper function that returns the POPULATION standard deviation
+  
+  # No input validation is required, in addition to what sd() already does
+  
 	return(sd(x, ...) * sqrt((length(x)-1)/length(x)))
 }
+
