148265c523435d0ffb4a538501f3c83c13b9b889
Maximilian Held <maximilian.held83@gmail.com>
2015-08-23 00:19:00 +0200

    have qmethod write out its rotation matrix to results #171

--- R/qmethod.R
+++ R/qmethod.R
@@ -7,11 +7,11 @@ qmethod <- function(dataset, nfactors, rotation="varimax", forced=TRUE, distribu
   thold.05 <- 1.96/sqrt(nstat)
   #check that the input data is correct
   #if (nqsorts!=ncol(dataset)) stop("Q method input: The number of Q sorts introduced does not match with the number of columns of the data frame or matrix") else if (nstat!=nrow(dataset)) stop("Q method input: The number of statements introduced does not match with the number of rows of the data frame or matrix.") else
-  
+
   # Validation checks
   if (nstat < 2) stop("Q method input: The data frame or matrix entered has less than two statements")
   if (nqsorts < 2) stop("Q method input: The data frame or matrix entered has less than two Q-sorts")
-  if (!is.numeric(as.matrix(dataset)) & !is.integer(as.matrix(dataset))) stop("Q method input: The data frame or matrix entered has non-numerical values.") 
+  if (!is.numeric(as.matrix(dataset)) & !is.integer(as.matrix(dataset))) stop("Q method input: The data frame or matrix entered has non-numerical values.")
   if (forced) {
     qscores <- sort(dataset[,1], decreasing=FALSE)
     if (sum(apply(dataset, 2, function(x) sort(x) != qscores)) > 0) stop("Q method input: The argument 'forced' is set as 'TRUE', but your data contains one or more Q-sorts that do not to follow the same distribution.")
@@ -22,15 +22,72 @@ qmethod <- function(dataset, nfactors, rotation="varimax", forced=TRUE, distribu
     if (!is.numeric(distribution) & !is.integer(distribution)) stop("Q method input: The distribution provided contains non-numerical values.")
   }
   if (length(unique(colnames(dataset))) != nqsorts) stop("Q method input: one or more Q-sort names are duplicated. Please change the names of the dataset by using colnames().")
-  
-  # Run the analysis
+
+  # Run the analysis ===========================================================
+
+  # find unrotated loadings
   cor.data <- cor(dataset, method=cor.method)
+  pca.results <- principal(r = cor.data, nfactors = nfactors, rotate = "none", n.obs = nrow(dataset), covar = FALSE)
+  loa.unrot <- unclass(pca.results$loadings)
+
+  # rotate loadings and store rot.mat
+  if (rotation == "none") {
+    loa <- loa.unrot  # just take the unrotated loadings
+    rot.mat <- diag(nfactors)  # if there is no rotation, an identity matrix is the rot.matrix
+  } else if (rotation == "varimax") {
+  # the following tries to replicate EXACTLY what psych::principal (v.1.5.6) does internally for legacy reasons
+  # prior versions of qmethod (< 1.4.0) here only used the rotation from within psych::principal
+  # separate rotation procedures were necessary in 1.4.0 to have a rot.mat as per https://github.com/aiorazabala/qmethod/issues/171
+  # from psych::principal doc:
+  #  > Rotations and transformations are either part of psych (Promax and cluster), of base R (varimax), or of GPArotation (simplimax, quartimax, oblimin).
+    varimax.results <- varimax(x = loa.unrot)
+    # using base::varimax, because psych::principal does
+    # see how in the psych::principal source, stats::varimax is also called with defaults
+    # > varimax = {rotated <- stats::varimax(loadings)  #varimax is from stats, the others are from GPArotation
+    # > loadings <- rotated$loadings},
+    loa <- unclass(varimax.results$loadings)
+    rot.mat <- varimax.results$rotmat
+  } else if (rotation == "promax") {  # this is done using psych functions as per manual
+    promax.results <- Promax(x = loa.unrot)
+    loa <- unclass(promax.results$loadings)
+    rot.mat <- promax.results$rotmat
+    # here too, psych::principal relies on defaults otherwise
+    # > cluster = 	 {loadings <- varimax(loadings)$loadings
+    # > pro <- target.rot(loadings)
+    # > loadings <- pro$loadings
+    # > Phi <- pro$Phi},
+  } else if (rotation == "cluster") {  # this is done using psych functions as per manual
+    cluster.results <- target.rot(x = varimax(x = loa.unrot)$loadings)
+    loa <- unclass(cluster.results$loadings)
+    rot.mat <- cluster.results$rotmat
+    # here too, psych::principal relies on defaults otherwise
+    # > cluster = 	 {loadings <- varimax(loadings)$loadings
+    # > pro <- target.rot(loadings)
+    # > loadings <- pro$loadings
+    # > Phi <- pro$Phi},
+  # remainder are all GPArotation procedures as per psych manual
+  } else if (rotation == "quartimax") {  # this is orthogonal
+    GPAresults <- GPForth(A = loa.unrot, method = rotation)
+    loa <- unclass(GPAresults$loadings)
+    rot.mat <- GPAresults$Th
+  } else if (rotation %in% c("simplimax", "oblimin")) {  # these are oblique
+    GPAresults <- GPFoblq(A = loa.unrot, method = rotation)
+    # notice that these procedures can occassionally NOT converge and fail
+    loa <- unclass(GPAresults$loadings)
+    rot.mat <- GPAresults$Th
+  } else {
+    stop("The chosen rotation is not available in qmethod.")
+  }
+
   loa <- as.data.frame(unclass(principal(cor.data, nfactors=nfactors, rotate=rotation, ...)$loadings)) #PCA from {psych} for factor loadings
   names(loa) <- paste0("f", 1:length(loa))
+  colnames(rot.mat) <- names(loa)  # name rotmat
+  rownames(rot.mat) <- names(loa)  # name rotmat
   # The following depends on the qmethod functions: qflag, qzscores, qfcharact, qdc
   flagged <- qflag(loa=loa, nstat=nstat)
   qmethodresults <- qzscores(dataset, nfactors, flagged=flagged, loa=loa, forced=forced, distribution=distribution)
   qmethodresults$brief$rotation <- rotation
+  qmethodresults$brief$rotmat <- rot.mat
   qmethodresults$brief$flagging <- "automatic"
   qmethodresults$brief$cor.method <- cor.method
   qmethodresults$brief$info <- c("Q-method analysis.",
