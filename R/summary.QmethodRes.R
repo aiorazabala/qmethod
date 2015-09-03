84ebe413efdecc093327f15606a7f155c3733d70
Maximilian Held <maximilian.held83@gmail.com>
2015-09-03 17:06:16 +0200

    make sure summary still works if order of elements changes

--- R/summary.QmethodRes.R
+++ R/summary.QmethodRes.R
@@ -1,14 +1,14 @@
 summary.QmethodRes <- function(object, ...) {
   cat(object$brief$info, sep="\n")
   cat("\nFactor scores\n")
-  print(object[[6]], quote=FALSE)
-  fch <- t(object[[7]][[1]])
+  print(object$zsc, quote = FALSE)
+  fch <- t(object$f_char$characteristics)
   rownames(fch) <- c(
-    "Average reliability coefficient", 
-    "Number of loading Q-sorts", 
-    "Eigenvalues", 
-    "Percentage of explained variance", 
-    "Composite reliability", 
+    "Average reliability coefficient",
+    "Number of loading Q-sorts",
+    "Eigenvalues",
+    "Percentage of explained variance",
+    "Composite reliability",
     "Standard error of factor scores")
-  print(fch, quote=FALSE, digits=2)
+  print(fch, quote = FALSE, digits=2)
 }
