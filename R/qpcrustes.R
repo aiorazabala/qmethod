ac65bacd43756a2e6b3ef638823cd9793f61a10e
aiorazabala <aiora.zabala@gmail.com>
2016-04-14 13:23:08 +0100

    namespace stuff

--- R/qpcrustes.R
+++ R/qpcrustes.R
@@ -1,7 +1,6 @@
 #Procrustes rotation for each bootstrap step, uses procrustes() function from MCMCpack
 qpcrustes <- function(loa, target, nfactors) {
     prox <- as.matrix(loa)
-    requireNamespace("MCMCpack")
     prores <- procrustes(prox, target)
     loarot <- as.data.frame(prores[1])
     colnames(loarot) <- paste("loarot_f", 1:nfactors, sep="")
